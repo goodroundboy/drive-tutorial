f82578cd95311106969d1949343af368e42707d0
August Terrell <august.terrell@aligent.com.au>
2025-02-20 17:08:25 +1030

    Folder IDs are now handled via routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@ import { db } from "~/server/db";
 import { files as filesSchema, folders as foldersSchema } from "~/server/db/schema";
 import DriveContents from "./drive-contents";
 
-export default async function GoogleDriveClone() {
-    const files = await db.select().from(filesSchema);
-    const folders = await db.select().from(foldersSchema);
+export default function HomePage() {
 
     return (
-        <DriveContents files={files} folders={folders} />
+        <div>Hello this is the home page</div>
     )
 }
